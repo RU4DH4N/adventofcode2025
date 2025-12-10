61ba7d9252b7d29566ca0a8394a53c80d8b69fda
RU4DH4N <76910193+RU4DH4N@users.noreply.github.com>
2025-12-10 20:59:46 +0000

    part 2

--- 1/src/main.cpp
+++ 1/src/main.cpp
@@ -6,26 +6,46 @@ int main() {
         std::cerr << "Error: Could not open resources/input.txt" << std::endl;
         return 1;
     }
-    
-    int x = 50;
-    int count = 0;
+
+    int x = 50; 
+    int count = 0; 
     std::string line;
-    
+
     while (std::getline(input, line)) {
         char direction = line[0];
-        int amount = std::stoi(line.substr(1));
-        
+        int amount = std::stoi(&line[1]); 
+        int remainder = amount;
+
         if (direction == 'L') {
-            x = (x + amount) % 100;
+            while (remainder > 0) {
+                int distanceToZero = (x == 0) ? 100 : x;
+
+                if (remainder >= distanceToZero) {
+                    count++;
+                    remainder -= distanceToZero;
+                    x = 0; 
+                } else {
+                    x = (x == 0) ? (100 - remainder) : (x - remainder);
+                    remainder = 0;
+                }
+            }
         } else {
-            x = (x - amount) % 100;
+            while (remainder > 0) {
+                int distanceToZero = (x == 0) ? 100 : (100 - x);
+
+                if (remainder >= distanceToZero) {
+                    count++;
+                    remainder -= distanceToZero;
+                    x = 0;
+                } else {
+                    x += remainder;
+                    remainder = 0;
+                }
+            }
         }
-        
-        if (x == 0) ++count;
     }
-    
+
     input.close();
     std::cout << count << std::endl;
-    
     return 0;
 }
